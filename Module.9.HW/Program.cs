7e5204acd570a2e8104895ba43c841209491e4ca
Name <email@example.ru>
2025-02-03 16:13:47 +0300

    Add project files.

---
+++ Module.9.HW/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module._9.HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
